6077afcaa57cc9c5ea507915253bda01418b7e69
João Gradim <joao.gradim@gmail.com>
2010-06-06 16:39:05 +0100

    Default image for APIs

--- xml2json/app/helpers/application_helper.rb
+++ xml2json/app/helpers/application_helper.rb
@@ -4,5 +4,9 @@ module ApplicationHelper
   def selected(action = "")
     action == action_name ? "selected" : ""
   end
+
+  def image_for_api(api, opts = {})
+    api.image.nil? ? image_tag("apis/default.png", opts) : image_tag(api.img, opts)
+  end
 end
 
