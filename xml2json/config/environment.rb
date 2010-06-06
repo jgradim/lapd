2bae8595e3da611353a49c290a0d80e533efd4ed
Nuno Polonia <nunopolonia@gmail.com>
2010-06-06 19:35:08 +0100

    authentication module completed

--- xml2json/config/environment.rb
+++ xml2json/config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'json'
+  config.gem "authlogic"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -27,7 +28,7 @@ Rails::Initializer.run do |config|
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]	
+  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
@@ -44,3 +45,4 @@ end
 require 'xml/xslt'
 require 'open-uri'
 require 'json/pure'
+
--- xml2json/config/routes.rb
+++ xml2json/config/routes.rb
@@ -1,16 +1,24 @@
 ActionController::Routing::Routes.draw do |map|
-  
+
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
+
+
+  map.resources :user_sessions
+  map.resources :users
+
   # resources
   map.resource :home, :only => :index, :collection => { :editor => :get, :convert => :get }
-  
+
   # named routes
   map.editor  '/editor',  :controller => 'home', :action => 'editor'
   map.convert '/convert.:format', :controller => 'home', :action => 'convert'
 
   # application root
   map.root :controller => 'home', :action => 'index'
-  
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  
+
 end
+
