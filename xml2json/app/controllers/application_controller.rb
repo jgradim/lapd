2f23c6bc00880f6d69b88810b0a3b002af099cb8
Nuno Polonia <nunopolonia@gmail.com>
2010-06-06 20:10:40 +0100

    admin area completed

--- xml2json/app/controllers/application_controller.rb
+++ xml2json/app/controllers/application_controller.rb
@@ -22,5 +22,13 @@ class ApplicationController < ActionController::Base
     @current_user = current_user_session && current_user_session.record
   end
 
+  def require_user
+    unless current_user
+      flash[:notice] = "You must be an admin to access this page"
+      redirect_to new_user_session_url
+      return false
+    end
+  end
+
 end
 
---
+++ xml2json/test/functional/apis_controller_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class ApisControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:apis)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create api" do
+    assert_difference('Api.count') do
+      post :create, :api => { }
+    end
+
+    assert_redirected_to api_path(assigns(:api))
+  end
+
+  test "should show api" do
+    get :show, :id => apis(:one).to_param
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, :id => apis(:one).to_param
+    assert_response :success
+  end
+
+  test "should update api" do
+    put :update, :id => apis(:one).to_param, :api => { }
+    assert_redirected_to api_path(assigns(:api))
+  end
+
+  test "should destroy api" do
+    assert_difference('Api.count', -1) do
+      delete :destroy, :id => apis(:one).to_param
+    end
+
+    assert_redirected_to apis_path
+  end
+end
