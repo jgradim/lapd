fa440de1d36033b133952caa0ccedb648730df6d
João Gradim <joao.gradim@gmail.com>
2010-06-06 20:21:54 +0100

    Fixed bug in image_for_api(@api)

--- xml2json/app/helpers/application_helper.rb
+++ xml2json/app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   end
 
   def image_for_api(api, opts = {})
-    api.image.present? ? image_tag("apis/default.png", opts) : image_tag(api.image, opts)
+    api.image.present? ? image_tag(api.image, opts) : image_tag("apis/default.png", opts)
   end
 end
 
